f90c411229a664141b3f24e6320a1bba842845e3
Rootdiv <rootdiv@mail.ru>
2021-01-21 14:36:05 +0300

    День 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,97 @@
+//Получем данный из БД
+class FetchData {
+  getResource = async url => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error('Произошла ошибка: ' + res.status)
+    }
+
+    return res.json();
+  }
+
+  getPost = async () => await this.getResource('db/dataBase.json');
+}
+
 class Twitter {
   constructor({
     listElem
   }) {
+    const fetchData = new FetchData()
     this.tweets = new Posts();
     this.elements = {
       listElem: document.querySelector(listElem)
     }
+
+    fetchData.getPost().then(data => {
+      data.forEach(this.tweets.addPost)
+      this.showAllPost()
+    });
   }
 
-  renderPosts() {
-
+  //Выводим посты в вёрстку
+  renderPosts(posts) {
+    this.elements.listElem.textContent = '';
+    posts.forEach(({
+      id,
+      userName,
+      nickname,
+      getDate,
+      text,
+      img,
+      likes,
+    }) => {
+      this.elements.listElem.insertAdjacentHTML('beforeend', `
+				<li>
+          <article class="tweet">
+            <div class="row">
+              <img class="avatar" src="images/${nickname}.jpg" alt="Аватар пользователя ${userName}">
+              <div class="tweet__wrapper">
+                <header class="tweet__header">
+                  <h3 class="tweet-author">${userName}
+                    <span class="tweet-author__add tweet-author__nickname">@${nickname}</span>
+                    <time class="tweet-author__add tweet__date">${getDate()}</time>
+                  </h3>
+                  <button class="tweet__delete-button chest-icon" data-id="${id}"></button>
+                </header>
+                <div class="tweet-post">
+                  <p class="tweet-post__text">${text}</p>
+                  ${img ? `
+                    <figure class="tweet-post__image">
+                      <img src="${img}"
+                        alt="иллюстрация из поста ${nickname}">
+                    </figure>` : 
+                    ''}
+                </div>
+              </div>
+            </div>
+            <footer>
+              <button class="tweet__like">
+                ${likes}
+              </button>
+            </footer>
+          </article>
+				</li>
+      `)
+    })
   }
 
+  //Показываем посты пользователя
   showUserPost() {
 
   }
 
+  //Показываем лайки пользователя
   showLikesPost() {
 
   }
 
+  //Показываем все посты пользователя
   showAllPost() {
-
+    this.renderPosts(this.tweets.posts)
   }
 
+  //Открываем модальное окно
   openModal() {
 
   }
@@ -36,10 +104,11 @@ class Posts {
     this.posts = posts;
   };
 
-  addPost(tweet) {
-    this.posts.push(new Post(tweet));
+  addPost = tweets => {
+    this.posts.push(new Post(tweets));
   };
 
+  //Удаляем пост
   deletePost(id) {
 
   };
@@ -50,26 +119,51 @@ class Posts {
 }
 
 class Post {
-  constructor(param) {
-    this.id = param.id;
-    this.username = param.username;
-    this.nickname = param.nickname;
-    this.postDate = param.postDate;
-    this.text = param.text;
-    this.img = param.img;
-    this.likes = param.likes;
+  constructor({
+    id,
+    userName,
+    nickname,
+    postDate,
+    text,
+    img,
+    likes = 0,
+  }) {
+    this.id = id || this.generateID();
+    this.userName = userName;
+    this.nickname = nickname;
+    this.postDate = postDate ? new Date(postDate) : new Date;
+    this.text = text;
+    this.img = img;
+    this.likes = likes;
     this.liked = false;
   };
 
+  //Удаляем/ставим лайк
   changeLike() {
     this.liked = !this.liked
     if (this.liked) this.likes++;
     else this.likes--;
   };
+
+  //Генерируем ID
+  generateID() {
+    return Math.random().toString(32).substring(2, 9) + (+new Date).toString(32);
+  };
+
+  //Получем дату
+  getDate = () => {
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+    }
+
+    return this.postDate.toLocaleString('ru-RU', options)
+  }
 }
 
 const twitter = new Twitter({
   listElem: '.tweet-list'
 })
-
-console.log('twitter: ', twitter);
