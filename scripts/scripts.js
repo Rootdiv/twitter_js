7da8244108967bff072af30dd04e84d72f2473e9
Rootdiv <rootdiv@mail.ru>
2021-01-22 14:00:14 +0300

    День 3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,30 +4,42 @@ class FetchData {
     const res = await fetch(url);
 
     if (!res.ok) {
-      throw new Error('Произошла ошибка: ' + res.status)
-    }
+      throw new Error('Произошла ошибка: ' + res.status);
+    };
 
     return res.json();
-  }
+  };
 
   getPost = async () => await this.getResource('db/dataBase.json');
-}
+};
 
 class Twitter {
   constructor({
-    listElem
+    user,
+    listElem,
+    modalElems,
+    tweetElems,
+    tweetPageElems,
   }) {
-    const fetchData = new FetchData()
+    const fetchData = new FetchData();
+    this.user = user;
     this.tweets = new Posts();
     this.elements = {
-      listElem: document.querySelector(listElem)
-    }
+      listElem: document.querySelector(listElem),
+      modal: modalElems,
+      tweetElems,
+      tweetPageElems,
+    };
 
     fetchData.getPost().then(data => {
-      data.forEach(this.tweets.addPost)
-      this.showAllPost()
+      data.forEach(this.tweets.addPost);
+      this.showAllPost();
     });
-  }
+
+    this.elements.modal.forEach(this.handlerModal, this);
+    this.elements.tweetElems.forEach(this.addTweet, this);
+    this.elements.tweetPageElems.forEach(this.addTweet, this);
+  };
 
   //Выводим посты в вёрстку
   renderPosts(posts) {
@@ -60,8 +72,8 @@ class Twitter {
                     <figure class="tweet-post__image">
                       <img src="${img}"
                         alt="иллюстрация из поста ${nickname}">
-                    </figure>` : 
-                    ''}
+                    </figure>` :
+                  ''}
                 </div>
               </div>
             </div>
@@ -72,30 +84,109 @@ class Twitter {
             </footer>
           </article>
 				</li>
-      `)
-    })
-  }
+      `);
+    });
+  };
 
   //Показываем посты пользователя
   showUserPost() {
 
-  }
+  };
 
   //Показываем лайки пользователя
   showLikesPost() {
 
-  }
+  };
 
   //Показываем все посты пользователя
   showAllPost() {
     this.renderPosts(this.tweets.posts)
-  }
+  };
 
-  //Открываем модальное окно
-  openModal() {
+  //Открываем/закрываем модальное окно
+  handlerModal({
+    button,
+    modal,
+    overlay,
+    close,
+  }) {
+    const buttonElem = document.querySelector(button);
+    const modalElem = document.querySelector(modal);
+    const overlayElem = document.querySelector(overlay);
+    const closeElem = document.querySelector(close);
 
+    const openModal = () => {
+      modalElem.style.display = 'block';
+    }
+
+    const closeModal = (elem, event) => {
+      const target = event.target;
+      if (target === elem) {
+        modalElem.style.display = 'none';
+      }
+    }
+
+    buttonElem.addEventListener('click', openModal);
+
+    if (closeElem) {
+      closeElem.addEventListener('click', closeModal.bind(null, closeElem));
+    }
+
+    if (overlayElem) {
+      overlayElem.addEventListener('click', closeModal.bind(null, overlayElem));
+    }
+
+    //Если открыто модальное окно, то закрываем его.
+    let modalOpen = modalElem.style.display;
+    if (modalOpen === 'block') {
+      this.handlerModal.closeModal = () => {
+        modalElem.style.display = 'none';
+      };
+    } else {
+      this.handlerModal.closeModal = () => {};
+    };
+  };
+
+  addTweet({
+    text,
+    img,
+    submit,
+  }) {
+    const textElem = document.querySelector(text);
+    const imgElem = document.querySelector(img);
+    const submitElem = document.querySelector(submit);
+
+    let imgUrl = '';
+    let tempString = textElem.innerHTML;
+
+    submitElem.addEventListener('click', () => {
+      //Запрет оправки пустого текста и временного текста
+      if (textElem.innerHTML !== '' && textElem.innerHTML !== tempString) {
+        this.tweets.addPost({
+          userName: this.user.name,
+          nickname: this.user.nick,
+          text: textElem.innerHTML,
+          img: imgUrl,
+        })
+        this.showAllPost();
+        this.handlerModal.closeModal();
+        textElem.innerHTML = tempString;
+        submitElem.style.opacity = 0.5;
+      }
+    });
+
+    textElem.addEventListener('click', () => {
+      if (textElem.innerHTML === tempString) {
+        textElem.innerHTML = '';
+        submitElem.style.opacity = 1;
+      }
+    });
+
+    imgElem.addEventListener('click', () => {
+      imgUrl = prompt('Введите адрес картинки: ');
+    });
   }
-}
+};
 
 class Posts {
   constructor({
@@ -116,7 +207,7 @@ class Posts {
   likePost(id) {
 
   };
-}
+};
 
 class Post {
   constructor({
@@ -158,12 +249,34 @@ class Post {
       day: 'numeric',
       hour: '2-digit',
       minute: '2-digit',
-    }
+    };
 
-    return this.postDate.toLocaleString('ru-RU', options)
-  }
-}
+    return this.postDate.toLocaleString('ru-RU', options);
+  };
+};
 
 const twitter = new Twitter({
-  listElem: '.tweet-list'
-})
+  listElem: '.tweet-list',
+  user: {
+    name: 'Владимир',
+    nick: 'rootdiv',
+  },
+  modalElems: [{
+    button: '.header__link_tweet',
+    modal: '.modal',
+    overlay: '.overlay',
+    close: '.modal-close__btn',
+  }],
+  //Элементы твита в модальном окне
+  tweetElems: [{
+    text: '.modal .tweet-form__text',
+    img: '.modal .tweet-img__btn',
+    submit: '.modal .tweet-form__btn',
+  }],
+  //Элементы твита на странице
+  tweetPageElems: [{
+    text: '.tweet-form__text',
+    img: '.tweet-img__btn',
+    submit: '.tweet-form__btn',
+  }],
+});
